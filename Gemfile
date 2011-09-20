3beb7cb329258e419197d5d3080d49658bd5c476
Jon-Paul Lussier <jonpaul.lussier@gmail.com>
2011-09-20 17:21:49 -0400

    First commit

---
+++ Gemfile
@@ -0,0 +1,22 @@
+source 'http://rubygems.org'
+
+gem 'rails', '3.1.0'
+# For EDGE do gem 'rails',     :git => 'git://github.com/rails/rails.git'
+gem 'jquery-rails'
+gem 'mysql2'
+
+group :assets do
+  gem 'sass-rails', "  ~> 3.1.0"
+  gem 'coffee-rails', "~> 3.1.0"
+  gem 'uglifier'
+end
+
+group :test, :development do
+  gem 'rspec-rails', '~> 2.6'
+end
+
+group :test do
+  gem 'cucumber-rails'
+  gem 'capybara'
+  gem 'database_cleaner'
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ticketea::Application.config.secret_token = '56b1e94847995ce98830dc5a01ad95d39c8526d3c5bf776b6e8b06e819a5796046a729f0fcbd3c3a626d32d5c573347c821c97a61a90e3261d59504415c0dd85'
