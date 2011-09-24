3cba8f8fa3cc7fa507e42e897b62a2b636304db5
Jon-Paul Lussier Jr <jp@rumbrook.net>
2011-09-24 16:14:05 -0400

    Projects have tickets and features for tickets

--- config/routes.rb
+++ config/routes.rb
@@ -1,19 +1,15 @@
 Ticketea::Application.routes.draw do
-  resources :projects
-  root :to => "projects#index"
-  # The priority is based upon order of creation:
   # first created -> highest priority.
+  resources :projects do
+    resources :tickets
+  end
 
   # Sample of regular route:
   #   match 'products/:id' => 'catalog#view'
-  # Keep in mind you can assign values other than :controller and :action
 
   # Sample of named route:
   #   match 'products/:id/purchase' => 'catalog#purchase', :as => :purchase
   # This route can be invoked with purchase_url(:id => product.id)
-
-  # Sample resource route (maps HTTP verbs to controller actions automatically):
-  #   resources :products
 
   # Sample resource route with options:
   #   resources :products do
@@ -57,4 +53,5 @@ Ticketea::Application.routes.draw do
   # This is a legacy wild controller route that's not recommended for RESTful applications.
   # Note: This route will make all actions in every controller accessible via GET requests.
   # match ':controller(/:action(/:id(.:format)))'
-end
+  root :to => "projects#index"
+  end
