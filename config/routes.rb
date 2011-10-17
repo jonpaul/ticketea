c937b4d51aa3e6f45394790425c6ffd0832162d7
Jon-Paul Lussier Jr <jp@rumbrook.net>
2011-10-16 22:49:07 -0400

    Added permissions screen to admin dashboard

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 Ticketea::Application.routes.draw do
+  # first created -> highest priority.
 
   devise_for :users, :controllers => { :registrations => "registrations" }
   get '/awaiting_confirmation',
       :to => "users#confirmation",
       :as => 'confirm_user'
 
-  # first created -> highest priority.
   resources :projects do
     resources :tickets
   end
@@ -13,6 +13,13 @@ Ticketea::Application.routes.draw do
   root :to => "projects#index"
   namespace :admin do
     root :to => "base#index"
-    resources :users
+    resources :users do
+      resources :permissions
+    end
   end
+
+  put '/admin/users/:user_id/permissions',
+                    :to => 'admin/permissions#update',
+                    :as => :update_user_permissions
+
 end
